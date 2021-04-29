a50b53e3d55e34bbb38bde40954b79e8c5608b35
Sergey Stolyarov <sergeystolatov@gmail.com>
2021-04-29 12:24:55 +0500

    28.04.2021

--- html css/PHP/index.php
+++ html css/PHP/index.php
@@ -1,28 +1,79 @@
-<?php 
-	$name = 'Сергей';
-	$age = 19;
-	$pi = pi();
-	$d = date("Y/m/d H:i:s");
-	$hour = date('H');
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Первое ДЗ по PHP</title>
-</head>
-<body>
-	<div><?php echo "Меня зовут $name"; ?></div>
-	<div><?php echo "Мой возраст $age лет."; ?></div>
-	<div><?php echo "Число Пи – математическая константа, которая выражает отношение длины окружности к её диаметру. Равна приблизительно: $pi "; ?></div>
-	<div><?php echo "Сегодняшняя дата $d"; ?></div>
-<?php 
-	if ( $hour >= 20 and $hour < 8 ) {
-		echo '<p style="background-color: SlateBlue;"">Применена Ночная тема</p>';
-	} elseif ($hour < 20 and $hour >= 8) {
-		echo '<p style="background-color: yellow;">Применена дневная тема</p>';
-	} else {
-		echo "loh";
+<?php
+//1
+echo "<strong>1 задача: вывод чисел от 5 до 13</strong><br>";
+for ($i=5; $i <= 13; $i++) { 
+	echo "$i <br>";
+}
+echo "<br>";
+
+//2
+
+// on while
+echo "<strong>2 задача с использованием цикла WHILE</strong><br>";
+$num = 1000;
+$i = 0;
+while ( $num >= 50 ) {
+	$num = intdiv($num, 2);
+	$i++;
+	echo "$i итерация: $num <br>";
+}
+echo "число итераций цикла WHILE = <strong>$i</strong> <br><br>";
+
+// on for
+echo "<strong>2 задача с использованием цикла FOR</strong><br>";
+for ( $num = 1000, $i = 1; $num > 50; $i++ ) {
+	$num = intdiv($num, 2);
+	echo "$i итерация: $num <br>";
+}
+echo " число итераций цикла FOR = <strong>". $i = $i-1 ."</strong> <br><br>";
+
+
+$i = $_POST['val'];
+if ( isset($_POST['btn']) and $_POST['btn'] == 'Вычислить' ) {
+	switch ($i) {
+		case '0':
+			echo " Результат выполнения: 1,2,3,4,5,6,7,8,9,10";
+			break;
+		case '1':
+			echo " Результат выполнения: 1,2,3,4,5,6,7,8,9";
+			break;
+		case '2':
+			echo " Результат выполнения: 1,2,3,4,5,6,7,8";
+			break;
+		case '3':
+			echo " Результат выполнения: 1,2,3,4,5,6,7";
+			break;
+		case '4':
+			echo " Результат выполнения: 1,2,3,4,5,6";
+			break;
+		case '5':
+			echo " Результат выполнения: 1,2,3,4,5";
+			break;
+		case '6':
+			echo " Результат выполнения: 1,2,3,4";
+			break;
+		case '7':
+			echo " Результат выполнения: 1,2,3";
+			break;
+		case '8':
+			echo " Результат выполнения: 1,2";
+			break;
+		case '9':
+			echo " Результат выполнения: 1";
+			break;
+		case '10':
+			echo " Результат выполнения: 0";
+			break;
+		
+		default:
+			echo "<p style='color: red;'>Вы ввели не правильное значение!</p>";
+			break;
 	}
-?> 
-</body>
-</html>
+}
+?>
+
+<form action="" method="post">
+	<label>Введите значение от 0 до 10</label>
+	<input type="text" name="val" value="" required>
+	<input type="submit" name="btn" value="Вычислить">
+</form>
